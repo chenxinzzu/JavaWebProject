b2f9da7c6482180c042a9d5eebb1bf3d520ae893
Chen Xin <chenxinzzu@163.com>
2015-12-23 22:08:12 +0800

    Implement most of features, but with the bug of only single access.

--- springLogin/src/main/java/com/example/controller/LoginController.java
+++ springLogin/src/main/java/com/example/controller/LoginController.java
@@ -4,11 +4,18 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 import org.iq80.leveldb.*;
+import org.iq80.leveldb.impl.Iq80DBFactory;
 import static org.iq80.leveldb.impl.Iq80DBFactory.*;
 import java.io.*;
 
+
+import java.nio.charset.Charset;
+
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -36,39 +43,80 @@ public class LoginController extends AbstractController {
 		this.failView = failView;
 	}
    
-  @Override
+  
+	@Override
   protected ModelAndView handleRequestInternal(HttpServletRequest request,
       HttpServletResponse response) throws Exception {
     //不应该是这样写，但是这样看起来比较容易理解
     String username = request.getParameter("username");
 	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
 	String time=df.format(new Date());
+	int count=0;
 	
-	Options options = new Options();
-	options.createIfMissing(true);
-	DB db = factory.open(new File("example"), options);
-	try {
-	  // Use the db in here....
-	   	 db.put(bytes("username"), bytes(username));
-	   	 db.put(bytes("time"), bytes(time));
-	     username = asString(db.get(bytes("username")));
-	     time=asString(db.get(bytes("time")));
-	} finally {
-	  // Make sure you close the db to shutdown the 
-	  // database and avoid resource leaks.
-	  db.close();
-	}
-	
-
     User user=new User(username,time);
-    //保存相应的参数，通过ModelAndView返回
-	Map<String ,Object> model=new HashMap<String,Object>();
-	if(username !=null && !username.equals("")){
-	model.put("user", user);
-		return new ModelAndView(getSuccessView(),model);
-	}else{
+	    //保存相应的参数，通过ModelAndView返回
+	Map<String ,String> model=new HashMap<String,String>();
+		
+	
+	boolean cleanup = false;
+    Charset charset = Charset.forName("utf-8");
+    String path = "E:/data/leveldb";
+
+    //init
+    DBFactory factory = Iq80DBFactory.factory;
+    File dir = new File(path);
+    //如果数据不需要reload，则每次重启，尝试清理磁盘中path下的旧数据。
+    if(cleanup) {
+        factory.destroy(dir,null);//清除文件夹内的所有文件。
+    }
+    Options options = new Options().createIfMissing(true);
+    //重新open新的db
+    DB db = factory.open(dir,options);
+
+    //write
+    if(username !=null && !username.equals("")){
+    	db.put(username.getBytes(charset),time.getBytes(charset));
+    	model.put(username, time);
+
+    }
+    else{
 		model.put("error", "Try again");
 		return new ModelAndView(getFailView(),model);
-	}		
+    }
+
+
+    //读取当前snapshot，快照，读取期间数据的变更，不会反应出来
+    Snapshot snapshot = db.getSnapshot();
+    //读选项
+    ReadOptions readOptions = new ReadOptions();
+    readOptions.fillCache(false);//遍历中swap出来的数据，不应该保存在memtable中。
+    readOptions.snapshot(snapshot);//默认snapshot为当前。
+    DBIterator iterator = db.iterator(readOptions);
+    while (iterator.hasNext()) {
+        Map.Entry<byte[],byte[]> item = iterator.next();
+        String key = new String(item.getKey(),charset);
+        String value = new String(item.getValue(),charset);//null,check.
+        System.out.println(key + ":" + value);
+      	model.put(key, value);
+        count++;
+    }
+    iterator.close();//must be
+ 
+    //
+    db.close();
+
+    
+	ModelAndView mav=new ModelAndView();
+	mav.addObject("model",model);
+	mav.addObject("user",user);
+	mav.addObject("count",count);
+	mav.setViewName("showUser");
+
+    return mav;
+	
+		//return new ModelAndView(getSuccessView(),model);
+  
+	}
+		
   }	
-}
+
--- springLogin/src/main/webapp/showUser.jsp
+++ springLogin/src/main/webapp/showUser.jsp
@@ -1,17 +1,24 @@
-<%@ page import="java.io.*,java.util.*" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>  
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%! int count = 0; %>
+      <% count++; %>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 </head>
 <body bgcolor="#D9DFAA">
-<h2>Hello,${user.username},你是第N个访问者！</h2>
+
+<h2>Hello,${user.username},你是第${count}个访问者！</h2>
+
 <table width="100%" border="1" align="center">
 <th>用户信息</th><th>登录时间</th>
-<tr><td>${user.username }</td><td>${user.time }</td></tr>
+<c:forEach items="${model}" var="node">  
+<tr><td><c:out value="${node.key}"></c:out></td>  
+ <td><c:out value="${node.value}"></c:out></td></tr> 
+</c:forEach>
 </table>
   <font color="red">欢迎登陆！！！</font>
 </body>
