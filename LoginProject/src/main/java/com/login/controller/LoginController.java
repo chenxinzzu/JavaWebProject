ebf2d56693d4bc5a79da2c3e061462041939fc75
Chen Xin <chenxinzzu@163.com>
2015-12-29 16:33:38 +0800

    Use DefaultAnnotationHandlerMapping for mapping, it's more convenient for configuration.

--- LoginProject/src/main/java/com/login/controller/LoginController.java
+++ LoginProject/src/main/java/com/login/controller/LoginController.java
@@ -13,17 +13,18 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-
+@Controller
 public class LoginController extends AbstractController {
-  
+  @RequestMapping(value="login.do")
   
 	@Override
   protected ModelAndView handleRequestInternal(HttpServletRequest request,
